0a8c05866b45ac947274326266bc4c1de3a3f24e
Aidan McKeown <aidan.mckeown@ad.aie.edu>
2016-08-08 11:19:18 +1000

    Shader fixed and test scene added

---
+++ Assets/TestCamera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCamera : MonoBehaviour {
+
+    bool plus = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GetComponent<Camera>().orthographicSize > 4)
+        {
+            plus = !plus;
+            GetComponent<Camera>().orthographicSize = 4;
+        }
+        if (GetComponent<Camera>().orthographicSize < 2.5f)
+        {
+            plus = !plus;
+            GetComponent<Camera>().orthographicSize = 2.5f;
+        }
+
+        if (plus)
+        {
+            GetComponent<Camera>().orthographicSize += Time.deltaTime * 0.5f;
+        }
+        else
+        {
+
+            GetComponent<Camera>().orthographicSize -= Time.deltaTime * 0.5f;
+        }
+
+    }
+}
