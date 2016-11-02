48a00e1686bea8777bc6fcb274434ad8fde9b682
Sean Volders <sean.volders@ad.aie.edu>
2016-11-02 14:00:15 +1100

    I don't remember what i changed before lunch

--- Assets/Scripts/MainMenuScripts/Controls.cs
+++ Assets/Scripts/MainMenuScripts/Controls.cs
@@ -18,7 +18,11 @@ public class Controls : MonoBehaviour {
             GamePadState one = GamePad.GetState(PlayerIndex.One);
             GamePadState two = GamePad.GetState(PlayerIndex.Two);
 
-            if (one.Buttons.B == ButtonState.Pressed || two.Buttons.B == ButtonState.Pressed)
+            if (one.Buttons.B == ButtonState.Pressed || two.Buttons.B == ButtonState.Pressed
+                || one.Buttons.X == ButtonState.Pressed || two.Buttons.X == ButtonState.Pressed
+                || one.Buttons.Y == ButtonState.Pressed || two.Buttons.Y == ButtonState.Pressed
+                || one.Buttons.Start == ButtonState.Pressed || two.Buttons.Start == ButtonState.Pressed
+                || one.Buttons.Back == ButtonState.Pressed || two.Buttons.Back == ButtonState.Pressed)
             {
                 GameObject.Find("Canvas").transform.FindChild("Controls").gameObject.SetActive(false);
                 open = false;
