7c12a3d6f7b04abeae19dc00f9613bb698d889b6
Sean Volders <sean.volders@ad.aie.edu>
2016-11-02 14:39:54 +1100

    Added 'B-to-go-back' functionality to the character selection screen

---
+++ Assets/Scripts/GoBackToCharSelect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class GoBackToCharSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GamePadState one = GamePad.GetState(PlayerIndex.One);
+        GamePadState two = GamePad.GetState(PlayerIndex.Two);
+
+        if (one.Buttons.B == ButtonState.Pressed || two.Buttons.B == ButtonState.Pressed)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Character Select");
+        }
+    }
+}
