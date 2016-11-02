363990fd80c06e0b0ab848441887e346cfd4823c
Sean Volders <sean.volders@ad.aie.edu>
2016-11-02 12:10:00 +1100

    Fixed sprites, controls menu almost working

---
+++ Assets/Scripts/MainMenuScripts/Controls.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class Controls : MonoBehaviour {
+
+    [SerializeField] private AudioClip buttonSound;
+    [SerializeField] private AudioClip pressSound;
+
+    private bool open = false;
+
+    public void Update()
+    {
+        if (open)
+        {
+            GetComponent<UnityEngine.UI.Button>().Select();
+
+            GamePadState one = GamePad.GetState(PlayerIndex.One);
+            GamePadState two = GamePad.GetState(PlayerIndex.Two);
+
+            if (one.Buttons.B == ButtonState.Pressed || two.Buttons.B == ButtonState.Pressed)
+            {
+                GameObject.Find("Controls").gameObject.SetActive(false);
+                open = false;
+            }
+        }
+    }
+
+    public void Start()
+    {
+        GameObject.Find("Controls").gameObject.SetActive(false);
+    }
+
+    public void Pressed()
+    {
+        GameObject.Find("Controls").gameObject.SetActive(true);
+        open = true;
+    }
+}
